ba374e9e45b35af7cfa9cd81e84ba7ec7b28a4cf
aabukhadir <abdallhabukhader@gmail.com>
2024-12-21 21:44:47 +0300

    add customer service to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,13 @@ import { AppService } from './app.service';
 import { PriceModule } from './price/price.module';
 import { DatabaseModule } from './database.module';
 import { UploadModule } from './upload/upload.module'; // Import the UploadModule
-
+import { CustomerModule } from './customer/customer.module'
 @Module({
   imports: [
     PriceModule,
     DatabaseModule,
     UploadModule, // Add UploadModule here
+    CustomerModule
   ],
   controllers: [AppController],
   providers: [AppService],
