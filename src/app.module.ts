e1333c4fced4030be089147980d182d675d99a9c
aabukhadir <abdallhabukhader@gmail.com>
2024-12-24 20:14:32 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,17 @@ import { AppService } from './app.service';
 import { PriceModule } from './price/price.module';
 import { DatabaseModule } from './database.module';
 import { UploadModule } from './upload/upload.module'; // Import the UploadModule
-import { CustomerModule } from './customer/customer.module'
+import { CustomerModule } from './customer/customer.module';
+import { UsersModule } from './users/users.module';
+import { QuoteModule } from './quote/quote.module';
 @Module({
   imports: [
     PriceModule,
     DatabaseModule,
     UploadModule, // Add UploadModule here
-    CustomerModule
+    CustomerModule,
+    UsersModule,
+    QuoteModule,
   ],
   controllers: [AppController],
   providers: [AppService],
